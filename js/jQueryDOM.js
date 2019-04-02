a95bc5fdfd470abacc72b0ae2491deae0d38fdfe
Max Reeves <maxreeves@Maxs-MacBook-Pro.local>
2019-04-03 09:06:10 +1100

    Project Complete

--- js/jQueryDOM.js
+++ js/jQueryDOM.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
 
-// $(currentMarker).hide();
-
 $(".board").on("click", function(){
 	if (currentMarker === "X"){
-	$(this).css("background-image", "url('images/close.svg')");
+		$(this).css("background-image", "url('images/close.svg')");
+		$(this).off("click");
+		$("h2").fadeIn("slow").html("Player Two Go!")
 } else if (currentMarker === "O") {
-	$(this).css("background-image", "url('images/circumference.svg')")
+		$(this).css("background-image", "url('images/circumference.svg')")
+		$(this).off("click")
+		$("h2").fadeIn("slow").html("Player One Go!")
 }
 	let boardNumber = $(this).attr('id');
-
 	let marker = currentMarker
-	console.log(marker);
-	$(this).html(marker)
-
 	makeMove(boardNumber);
+});
 
+$(".board").on("click", function(){
+	if (numberOfTurns === 9){
+		$("h2").html("It's a Draw!")
+ }
+});
 
-
+$(".board").on("click", function(){
+	if (checkForWinner() === true && currentMarker === "O"){
+	$("h2").html(`Player One You Win`);
+	$(".board").off("click");
+}	else if (checkForWinner() === true && currentMarker === "X"){
+	$("h2").html(`Player Two You Win`);
+	$(".board").off("click");
+}
 });
 });
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,19 +1,6 @@
-// Board  1  2  3
-// .....  4  5  6
-// .....  7  8  9
-// ..............
-
-// Array numbers represent a tile on the game board
-// When an index is picked the player is switched until the 6th turn
-// then a function that checks the combination of these numbers runs
-// Three tiles in a row are considered a win so.. 1 2 3,, 4 5 6,, 7 8 9
-// 1 4 7,, 2 5 8,, 3 9 6,, 1 5 9,, 3 5 7.
-
 var board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let currentMarker = "X";
-var turnCounter = 0;
-
-// const
+var currentMarker = "X";
+var numberOfTurns = 0;
 
 const makeMove = function (boardNumber) {
   board[boardNumber-1] = currentMarker;
@@ -22,8 +9,9 @@ const makeMove = function (boardNumber) {
 };
 
 const swapMarker = function () {
+	numberOfTurns += 1;
 	if (currentMarker === "X"){
-		currentMarker = "O"
+		currentMarker = "O";
 		return "X";
 	} else {
 		currentMarker = "X";
@@ -32,35 +20,21 @@ const swapMarker = function () {
 };
 
 const checkForWinner = function () {
-	console.log(board);
-	if (board[0] === board[1] && board[0] === board[2]) {
-		console.log(`${ currentMarker } is the winner!`);
+		if      (board[0] === board[1] && board[0] === board[2]) {
+			return true;
 	} else if (board[3] === board[4] && board[3] === board[5]) {
-		console.log(`${ currentMarker } is the winner!`);
+			return true;
 	} else if (board[6] === board[7] && board[6] === board[8]) {
-		console.log(`${ currentMarker } is the winner!`);
+			return true;
 	} else if (board[0] === board[3] && board[0] === board[6]) {
-		console.log(`${ currentMarker } is the winner!`);
+			return true;
 	} else if (board[1] === board[4] && board[1] === board[7]) {
-		console.log(`${ currentMarker } is the winner!`);
+			return true;
 	} else if (board[2] === board[8] && board[2] === board[5]) {
-		console.log(`${ currentMarker } is the winner!`);
+			return true;
 	} else if (board[0] === board[4] && board[0] === board[8]) {
-		console.log(`${ currentMarker } is the winner!`);
+			return true;
 	} else if (board[2] === board[4] && board[2] === board[6]) {
-		console.log(`${ currentMarker } is the winner!`);
+			return true
 	}
 };
-
-
-// Board  1  2  3
-// .....  4  5  6
-// .....  7  8  9
-// ..............z
-
-//X wins
-// makeMove(0);
-// makeMove(4);
-// makeMove(3);
-// makeMove(7);
-// makeMove(6);
